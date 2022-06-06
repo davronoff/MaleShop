18194e00170bc95a825273b2936dec3d4fb5610a
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2022-06-06 16:00:11 +0500

    Add project files.

---
+++ MaleFashion/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MaleFashion.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult BlogDetails()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Main()
+        {
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
+        public IActionResult Shopdetails()
+        {
+            return View();
+        }
+        public IActionResult Shoppingcart()
+        {
+            return View();
+        }
+    }
+}
